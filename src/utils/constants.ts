cc5e0b7119d352574c5b3dd29a955c35dcb2aeff
massifrg <massifrg@gmail.com>
2026-02-17 12:38:57 +0100

    fix: in win32, use Get-WmiObject instead of deprecated wmic

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,20 +2,19 @@
  * Class with constants used in the application.
  */
 export class Constants {
+  /**
+   * Command to execute on Windows.
+   */
+  // public static readonly WINDOWS_COMMAND: string = 'wmic logicaldisk get Caption,FreeSpace,Size,VolumeSerialNumber,Description  /format:list';
+  public static readonly WINDOWS_COMMAND: string =
+    "Get-WmiObject -Class Win32_LogicalDisk | Select-Object Caption, FreeSpace, Size, VolumeSerialNumber, Description | Format-List";
+  /**
+   * Command to execute on Linux.
+   */
+  public static readonly LINUX_COMMAND: string = "df -P | awk 'NR > 1'";
 
-    /**
-     * Command to execute on Windows.
-     */
-    public static readonly WINDOWS_COMMAND: string = 'wmic logicaldisk get Caption,FreeSpace,Size,VolumeSerialNumber,Description  /format:list';
-
-    /**
-     * Command to execute on Linux.
-     */
-    public static readonly LINUX_COMMAND: string = 'df -P | awk \'NR > 1\'';
-
-    /**
-     * Command to execute on OSX.
-     */
-    public static readonly DARWIN_COMMAND: string = 'df -P | awk \'NR > 1\'';
-
+  /**
+   * Command to execute on OSX.
+   */
+  public static readonly DARWIN_COMMAND: string = "df -P | awk 'NR > 1'";
 }
